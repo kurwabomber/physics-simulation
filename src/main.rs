d770629000bf2f8af7db4cb0896f791d6e8d722b
Norad <davidWnguyen@proton.me>
2022-12-08 07:58:27 -0500

    a

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@ use crate::stocks::get_user_number;
 mod vector_addition;
 mod stocks;
 
+const APPLICATIONS: &'static [&'static str] = &["1. Vector Addition", "2. Planar Movement"];
+
 fn main() {
     loop{
-        let num:i32 = get_user_number(format!("\nList of Applications (type to play):\n1. Vector Addition"));
+        println!("\nList of Applications");
+        for key in APPLICATIONS{
+            println!("{}", key);
+        }
+        let num:i32 = get_user_number("".to_string());
         match num {
             1 => vector_addition::solve_vector_addition(),
             _ => println!("Try an actual selection this time."),
