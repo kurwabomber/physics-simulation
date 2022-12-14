73c798d2bd3d6b94d5e6a0e7e584391bb7997e88
Norad <davidWnguyen@proton.me>
2022-12-13 21:00:58 -0500

    kine

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod vector_addition;
 mod planar_movement;
 mod stocks;
 
-const APPLICATIONS: &'static [&'static str] = &["1. Vector Addition", "2. Planar Movement", ""];
+const APPLICATIONS: &'static [&'static str] = &["1. Vector Addition", "2. Planar Movement", "3. Kinematic Equations"];
 
 fn main() {
     loop{
@@ -16,6 +16,7 @@ fn main() {
         match num {
             1 => vector_addition::solve_vector_addition(),
             2 => planar_movement::solve_planar_movement(),
+            3 => planar_movement::solve_kinematics(),
             _ => println!("Try an actual selection this time."),
         }
     }
--- src/planar_movement.rs
+++ src/planar_movement.rs
@@ -1,7 +1,7 @@
 use crate::stocks::get_user_number;
 pub fn solve_planar_movement() {
     println!("||| PLANAR MOVEMENT |||\nInsert -1 to solve algebraically.");
-    //Get our two vectors
+    //$WAG
     let mut acceleration:f64 = get_user_number(format!("What's the acceleration?"));
     let mut velocity:f64 = get_user_number(format!("What's the initial velocity?"));
     let mut time:f64 = get_user_number(format!("How long is the duration?"));
@@ -10,17 +10,14 @@ pub fn solve_planar_movement() {
     if acceleration == -1.0
     {
         acceleration = ((displacement*2.0/time)-velocity*2.0)/time;
-        println!("||| Solution |||\nTime = {}s\nVelocity = {}m/s\nAcceleration = {}m/s^2\nDisplacement = {}m", time, velocity, acceleration, displacement);
     }
     else if velocity == -1.0
     {
         velocity = ((displacement*2.0/time)-time*acceleration)/2.0;
-        println!("||| Solution |||\nTime = {}s\nVelocity = {}m/s\nAcceleration = {}m/s^2\nDisplacement = {}m", time, velocity, acceleration, displacement);
     }
     else if time == -1.0
     {
         time = ((-2.0*velocity) - f64::sqrt((velocity*2.0).powf(2.0) - (4.0*acceleration) * (displacement * -2.0)))/(2.0*acceleration);
-        println!("||| Solution |||\nTime = {}s\nVelocity = {}m/s\nAcceleration = {}m/s^2\nDisplacement = {}m", time, velocity, acceleration, displacement);
     }
     else
     {
@@ -30,6 +27,64 @@ pub fn solve_planar_movement() {
         }
 
         displacement = (2.0*velocity + time*acceleration)/2.0 * 3.0;
-        println!("||| Solution |||\nTime = {}s\nVelocity = {}m/s\nAcceleration = {}m/s^2\nDisplacement = {}m", time, velocity, acceleration, displacement);
     }
+
+    println!("||| Solution |||\nTime = {}s\nVelocity = {}m/s\nAcceleration = {}m/s^2\nDisplacement = {}m", time, velocity, acceleration, displacement);
+}
+
+pub fn solve_kinematics() {
+    println!("||| KINEMATICS |||\nInsert -1 to solve algebraically.");
+    //Based on the missing variables, get the other values.
+    let mut acceleration:f64 = get_user_number(format!("What's the acceleration?"));
+    let mut velocity:f64 = get_user_number(format!("What's the initial velocity?"));
+    let mut fvelocity:f64 = get_user_number(format!("What's the final velocity?"));
+    let mut time:f64 = get_user_number(format!("How long is the duration?"));
+    let mut displacement:f64 = get_user_number(format!("How far did it travel?"));
+
+    //equation one, vf = v0 + at
+    if fvelocity == -1.0 && velocity != -1.0 && acceleration != -1.0 && time != -1.0
+    {
+        fvelocity = velocity + acceleration * time;
+    }
+    else if velocity == -1.0 && fvelocity != -1.0 && acceleration != -1.0 && time != -1.0
+    {
+        velocity = fvelocity - acceleration * time;
+    }
+    else if acceleration == -1.0 && fvelocity != -1.0 && velocity != -1.0 && time != -1.0
+    {
+        acceleration = (fvelocity-velocity)/time;
+    }
+    else if time == -1.0 && fvelocity != -1.0 && acceleration != -1.0 && velocity != -1.0
+    {
+        time = (fvelocity-velocity)/acceleration;
+    }
+    //equation two, displacement = ((vf+v0)/2)t
+    if displacement == -1.0 && fvelocity != -1.0 && velocity != -1.0 && time != -1.0
+    {
+        displacement = ((fvelocity+velocity)/2.0)*time;
+    }
+    else if fvelocity == -1.0 && displacement != -1.0 && velocity != -1.0 && time != -1.0
+    {
+        fvelocity = ((displacement*2.0)/time)-velocity;
+    }
+    else if velocity == -1.0 && displacement != -1.0 && fvelocity != -1.0 && time != -1.0
+    {
+        velocity = ((displacement*2.0)/time)-fvelocity;
+    }
+    else if time == -1.0 && displacement != -1.0 && fvelocity != -1.0 && velocity != -1.0
+    {
+        time = displacement/((fvelocity+velocity)/2.0);
+    }
+    //equation three, displacement = v0*t + (acceleration*time^2)
+    if displacement == -1.0 && velocity != -1.0 && time != -1.0 && acceleration != 1.0
+    {
+        displacement = (velocity*time) + (acceleration*f64::powf(time, 2.0))/2.0;
+    }
+    //equation four, vf^2 = v0^2 + 2*acceleration*displacement
+    if fvelocity == -1.0 && velocity != -1.0 && acceleration != -1.0 && displacement != -1.0
+    {
+        fvelocity = f64::sqrt(f64::powf(velocity, 2.0) + (2.0*acceleration*displacement));
+    }
+
+    println!("||| Solution |||\nTime = {}s\nInitial Velocity = {}m/s\nFinal Velocity = {}m/s\nAcceleration = {}m/s^2\nDisplacement = {}m", time, velocity, fvelocity, acceleration, displacement);
 }
